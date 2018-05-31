e8dc4674e79ecf503575c518efe1a3b912532a4e
philip <24674672@student.g.nwu.ac.za>
2018-05-31 18:19:21 +0200

    added listview - must now populate it

--- bitwatcher/app/src/main/java/com/test/bitwatcher/CryptoResultView.java
+++ bitwatcher/app/src/main/java/com/test/bitwatcher/CryptoResultView.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CryptoResultView extends AppCompatActivity {
+public class CryptoResultView extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,4 +25,10 @@ public class CryptoResultView extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onClick(View v){
+
+    }
+
 }
